5ac043c47e834c46d3e64ed68b40ca76bca68327
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-08-28 09:32:26 -0700

    ade some fix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+	int j;
+
+	for (n = 0; *s != '\0'; s++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				n++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (n);
+			}
+		}
+	}
+	return (n);
+}
