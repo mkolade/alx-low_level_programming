151eceb8997f02210b70c345613bb25be50bc6c0
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-08-28 09:12:56 -0700

    ok

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (0);
+}
