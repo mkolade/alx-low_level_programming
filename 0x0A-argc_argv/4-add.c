88585aa4f0771acdd2c3d60fc1a0fd74068f84ef
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-09-03 02:52:51 -0700

    ran script ./add 1 10 100 1000 successfuly

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	for (count = 0; count < strlen(str); count++)
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int count, str_to_int;
+
+	for (count = 1; count < argc; count++)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
