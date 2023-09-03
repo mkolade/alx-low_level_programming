f2ed74445a0ec33d01841c2efb19094502b4d1c4
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-09-03 01:45:58 -0700

    aded new argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function to print
+ * @argv: array of arguments
+ * @argc: number of arguents
+ * Return: Always (0) success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
