a698c4a0faaac10054c618c6dfa3d67f984220f4
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-09-11 13:14:19 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c -o c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#ifndef PRINT_FILE_NAME
-#define PRINT_FILE_NAME() printf("%s\n", __FILE__)
-#endif
 /**
  * main - function to print file name
  * Return: Always (0) success
  */
 int main()
 {
-	PRINT_FILE_NAME();
+	printf("%s\n", __FILE__);
 	return (0);
 }
