04dde91b90c66f6872be17371f8ec7b35fed1749
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-09-12 06:19:02 -0700

    ok

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - structure for a dog property
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
