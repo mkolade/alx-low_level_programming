822eb30a3c85ed081d135d679b1367c90c82067d
Mohammed Moshood <mohammedamoshood@gmail.com>
2023-09-13 21:25:59 -0700

    ok

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ typedef struct dog
 } dog_t;
 
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+int _strlen(char *s)i;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
